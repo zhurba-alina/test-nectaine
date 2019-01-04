a79e87f317b453baed07cbb198ac7a1cd4dc0e69
Alina Zhurba <zhurba.alina@gmail.com>
2019-01-04 16:35:11 +0200

    add check Demand upload

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
 
 class TestLogin():
     @pytest.fixture()
@@ -29,12 +30,12 @@ class TestLogin():
         driver.find_element_by_id('login').send_keys("admin")
         driver.find_element_by_id('password').send_keys("admin")
         driver.find_element_by_id('loginSubmitAuth').click()
-        wait = WebDriverWait(driver, 30)
+        wait = WebDriverWait(driver, 10)
+        driver.implicitly_wait(10)
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'nc-sidebar__logo__logo-normal')))
         print(" >> Test Login << ")
 
         # >> DEMAND PLANING <<
-        driver.implicitly_wait(30)
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'nc-sidebar__logo__triangle')))
         driver.find_element_by_class_name('nc-sidebar__logo__triangle').click()
         button_nectaine = driver.find_element_by_xpath('//div[contains(text(), "Nectain") and @class="nc-sidebar__desktops__item"]')
@@ -60,50 +61,47 @@ class TestLogin():
              '//span[@class="x-btn-icon-el  x-btn-glyph" and @role="presentation" and @id="button-1082-btnIconEl"]')))
         button_import_from_exel.click()
         print(" >> Test Demand Open << ")
-        button_upload = driver.find_element_by_xpath('//div[@class="el-upload-dragger"]')
         wait.until(EC.visibility_of_element_located(
             (By.XPATH,
              '//div[@class="el-upload-dragger"]')))
-
-        path_to_file = 'C:/Users/zhurba/PycharmProjects/test-nectaine/tests/test-bdg.xlsx'
+        path_to_file = '/home/alina/PycharmProjects/test-nectaine/tests/test-bdg.xlsx'
         driver.find_element_by_xpath('//input[@class="el-upload__input"]').send_keys(path_to_file)
-
         wait.until(EC.visibility_of_element_located(
             (By.XPATH,
              '//span[@class="el-dialog__title" and contains(text(), "Select a sheet")]')))
         driver.find_element_by_xpath('//*[@class="el-radio__inner"]').click()
         driver.find_element_by_xpath('//*[@class="el-dialog__body"]//*[@type="button"]').click()
-        driver.implicitly_wait(20)
         wait.until(EC.visibility_of_element_located(
             (By.XPATH,
              '//button[@class="excel-import__finish-btn el-tooltip"]')))
         wait.until(EC.element_to_be_clickable(
             (By.XPATH,
              '//button[@class="excel-import__finish-btn el-tooltip"]')))
-        driver.implicitly_wait(20)
         while True:
             try:
                 driver.find_element_by_xpath('//button[@class="excel-import__finish-btn el-tooltip"]').click()
-                wait.until(EC.visibility_of_element_located(
+                x = wait.until(EC.visibility_of_element_located(
                     (By.XPATH, '//div[@class="nc__popup__title"]')))
-                assert "Upload confirmation" in driver.find_element_by_xpath('//div[@class="nc__popup__title"]').text
-            except NoSuchElementException:
-                break
+                if x.is_displayed():
+                    assert "Upload confirmation" in driver.find_element_by_xpath(
+                        '//div[@class="nc__popup__title"]').text
+                    break
+            except TimeoutException:
+                continue
 
-        wait.until(EC.visibility_of_element_located(
-            (By.XPATH, '//div[@class="nc__popup__title"]')))
-        assert "Upload confirmation" in driver.find_element_by_xpath('//div[@class="nc__popup__title"]').text
         driver.find_element_by_xpath('//button[@class="nc__btn nc__btn__default"]').click()
 
-        wait.until(EC.element_to_be_clickable(
+        # wait.until(EC.text_to_be_present_in_element_value(
+        #     (By.XPATH, '//div[@role="alert" and @class="el-message el-message--success is-closable"]'),
+        #     "Data successfully added!"))
+
+        wait.until(EC.visibility_of_element_located(
             (By.XPATH,
              '//div[contains(text(), "Demand Items") and @class="nc__tab__title"]')))
-        # driver.find_element_by_xpath('//div[contains(text(), "Demand Items") and @class="nc__tab__title"]').click()
+
 
 
         print(" >> Test Demand Upload << ")
-
-
         driver.implicitly_wait(10)
         print(" >> Test Demand << ")
 
